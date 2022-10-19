37ea265bf69ae6745e29440b4e962faab206f4a6
Kisat0 <ghanembenyoucef@gmail.comm>
2022-10-19 22:21:29 +0200

    update vérification mdp

--- assets/php/includes/database.inc.php
+++ assets/php/includes/database.inc.php
@@ -1,24 +1,22 @@
 <?php
-class database{
+class database
+{
     public $bdd;
 
     public function connectBdd()
     {
-      if($this->bdd === null){
-          try {
-            $this->bdd = new PDO('mysql:host=localhost;dbname=puissance-4', "root", "");
-        
-          } catch (PDOException $e) {
-            print "Erreur !: " . $e->getMessage() . "<br/>";
-            die();
-          }
-          return $this->bdd;
-      }
+        if ($this->bdd === null) {
+            try {
+                $dsn = 'mysql:host=192.168.64.2;dbname=puissance-4';
+                $user = 'root';
+                $password = '';
+
+                $this->bdd = new PDO($dsn,$user,$password); 
+            } catch (PDOException $e) {
+                print "Erreur !: " . $e->getMessage() . "<br/>";
+                die();
+            }
+            return $this->bdd;
+        }
     }
 }
-
-
-
-
-
-?>
