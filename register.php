0923c4884723d0903491734ad8f6facfaf2396c1
Empeurman <eduardoccerqueira778@gmail.com>
2022-10-26 17:58:36 +0200

    fix

--- register.php
+++ register.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
     <link rel="stylesheet" href="assets/css/header.css">
-    <link rel="stylesheet" href="assets/css/register-login.css">
+    <link rel="stylesheet" href="assets/css/register.css">
     <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <?php
